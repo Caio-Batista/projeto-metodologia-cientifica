070a1c905fa261c76c5375f45b38fda877f60d8c
Caio Oliveira <caiobo@lsd.ufcg.edu.br>
2016-02-20 12:32:23 -0300

    Comecando a passar argumentos de um script para o outro

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Projeto de metodologia cientifica 2015.2
 Para rodar o script, use no terminal o comando:
 
-./classificar
+./classificar ARG1 ARG2 ARG3
--- classificar.py
+++ classificar.py
@@ -1,1 +1,9 @@
-print "Hello world!"
+# coding: utf-8
+import sys
+
+result = []
+
+for arg in sys.argv:
+    result.append(arg)
+
+print "Os argumentos estão sendo passados corretamente:  \n %s " % (str(result))
